db0ac71c19f3b4815965a079f585cb5884be92ed
Gustavo Becattini <80481089+gusbecattini@users.noreply.github.com>
2023-06-23 11:48:58 -0300

    Add files via upload

---
+++ BtalhaNaval/JogadorComputador.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+/* 
+ * 
+ * 
+ * 
+ * 
+ * namespace BatalhaNaval
+{
+    class JogadorComputador
+    {
+        //private TabuleiroBatalhaNaval tabuleiro;
+        private int pontuacao;
+        private Posicao posicao; 
+
+        public int Pontuacao
+        {
+            get { return pontuacao; }
+            set { pontuacao = value; }
+        }
+
+        /* public TabuleiroBatalhaNaval Tabuleiro 
+         {
+            get {return tabuleiro;}
+            set {tabuleiro = value;}         
+         }
+
+         
+
+        public bool ReceberAtaque(Posicao posicao)
+        {
+            
+            
+        }
+
+
+
+    }
+} */
