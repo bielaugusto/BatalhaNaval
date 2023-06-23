a6c3a228e99d9758f4b266152d2791e0d2c5a074
Gustavo Becattini <80481089+gusbecattini@users.noreply.github.com>
2023-06-23 11:45:00 -0300

    Add files via upload

---
+++ BtalhaNaval/Embarcacao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BatalhaNaval
+{
+    class Embarcacao
+    {
+        private string nome;
+        private int tamanho;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Tamanho
+        {
+            get { return tamanho; }
+            set { tamanho = value; }
+        }
+           
+        
+        //construtor da classe Embarcacao
+        public Embarcacao(string nome, int tamanho)
+        {
+            this.nome = nome;
+            this.tamanho = tamanho;
+        }
+
+
+    }
+}
