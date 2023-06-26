aadd8772721ebb502fcf66e3f8d68e488684fd7a
Gabriel Augusto <82222254+bielaugusto@users.noreply.github.com>
2023-06-26 17:43:25 -0300

    Update TabuleiroBatalhaNaval.cs

--- BtalhaNaval/TabuleiroBatalhaNaval.cs
+++ BtalhaNaval/TabuleiroBatalhaNaval.cs
@@ -2,88 +2,85 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BatalhaNaval
+class TabuleiroBatalhaNaval
 {
-    class TabuleiroBatalhaNaval
+    private int numLinhas;
+    private int numColunas;
+    private char[,] tabuleiro;
+
+    //Função para zerar o tabuleiro e settar como padrão 'A' para todas as posições
+    private void zeraMatriz(char[,] tabuleiro) {
+        for (int i = 0; i <= NumLinhas; i++)
+        {
+            for (int j = 0; j <= numColunas; j++)
+            {
+                tabuleiro[i, j] = 'A'; //agua
+            }
+        }
+    }
+
+
+    public TabuleiroBatalhaNaval(int numLinhas, int numColunas)
     {
-        private int numLinhas;
-        private int numColunas;
-        private char[,] tabuleiro;
+        this.numLinhas = numLinhas;
+        this.numColunas = numColunas;
+        tabuleiro = new char[numLinhas, numColunas];
 
-        //Função para zerar o tabuleiro e settar como padrão 'A' para todas as posições
-        private void zeraMatriz(char[,] tabuleiro) {
-            for (int i = 0; i <= tabuleiro.GetLength(0); i++)
+        zeraMatriz(tabuleiro);
+    }
+
+    public char[,] Tabuleiro { get; set; }
+    public int NumLinhas {
+        get { return numLinhas; }
+        set { numLinhas = value; }
+    }
+    public int NumColunas { 
+        get { return numColunas; }
+        set { numColunas = value; }
+    }
+
+    //Funções para imprimir os tabuleiros para os jogadores
+    public void imprimirTabuleiroJogador(char[,] tabuleiro) {
+        for (int i = 0; i <= numLinhas; i++)
+        {
+            for (int j = 0; j <= numColunas; j++)
             {
-                for (int j = 0; j <= tabuleiro.GetLength(1); j++)
-                {
-                    tabuleiro[i, j] = 'A';
-                }
+                Console.WriteLine(tabuleiro[i, j] + " ");
+            }
+        }Console.WriteLine(" ");
+    }
+
+    public void imprimirTabuleiroAdversario(char[,] tabuleiro)
+    {
+        for (int i = 0; i <= numLinhas; i++)
+        {
+            for (int j = 0; j <= numColunas; j++)
+            {
+                Console.WriteLine(tabuleiro[i, j] + " ");
             }
         }
+        Console.WriteLine(" ");
+    }
 
+    public bool AdicionarEmbarcacao(Embarcacao embarcacao, Posicao posicao)
+    {
+        // Verifica se a embarcação ultrapassa as dimensões do tabuleiro
+        if (posicao.Linha + embarcacao.Tamanho > numLinhas || posicao.Coluna + embarcacao.Tamanho > numColunas)
+            return false;
 
-        public TabuleiroBatalhaNaval(int numLinhas, int numColunas)
+        // Verifica se as posições onde a embarcação será adicionada já estão ocupadas
+        for (int i = 0; i < embarcacao.Tamanho; i++)
         {
-            this.numLinhas = numLinhas;
-            this.numColunas = numColunas;
-            tabuleiro = new char[numLinhas, numColunas];
-
-            zeraMatriz(tabuleiro);
-        }
-
-        public int NumLinhas {
-            get { return numLinhas; }
-            set { numLinhas = value; }
-        }
-        public int NumColunas { 
-            get { return numColunas; }
-            set { numColunas = value; }
-        }
-
-        //Funções para imprimir os tabuleiros para os jogadores
-        public char[,] imprimirTabuleiroJogador(char[,] tabuleiro) {
-            for (int i = 0; i <= tabuleiro.GetLength(0); i++)
-            {
-                for (int j = 0; j <= tabuleiro.GetLength(1); j++)
-                {
-                    Console.WriteLine(tabuleiro[i, j]);
-                }
-            }Console.WriteLine(" ");
-            return tabuleiro;
-        }
-
-        public char[,] imprimirTabuleiroAdversario(char[,] tabuleiro)
-        {
-            for (int i = 0; i <= tabuleiro.GetLength(0); i++)
-            {
-                for (int j = 0; j <= tabuleiro.GetLength(1); j++)
-                {
-                    Console.WriteLine(tabuleiro[i, j]);
-                }
-            }
-            return tabuleiro;
-        }
-
-        public bool AdicionarEmbarcacao(Embarcacao embarcacao, Posicao posicao)
-        {
-            // Verifica se a embarcação ultrapassa as dimensões do tabuleiro
-            if (posicao.Linha + embarcacao.Tamanho > numLinhas || posicao.Coluna + embarcacao.Tamanho > numColunas)
+            if (tabuleiro[posicao.Linha, posicao.Coluna + i] != 'A')
                 return false;
-
-            // Verifica se as posições onde a embarcação será adicionada já estão ocupadas
-            for (int i = 0; i < embarcacao.Tamanho; i++)
-            {
-                if (tabuleiro[posicao.Linha, posicao.Coluna + i] != 'A')
-                    return false;
-            }
-
-            // Adiciona a embarcação no tabuleiro
-            for (int i = 0; i < embarcacao.Tamanho; i++)
-            {
-                tabuleiro[posicao.Linha, posicao.Coluna + i] = 'B'; // 'B' representa barco
-            }
-
-            return true;
         }
+
+        // Adiciona a embarcação no tabuleiro
+        for (int i = 0; i < embarcacao.Tamanho; i++)
+        {
+            tabuleiro[posicao.Linha, posicao.Coluna + i] = 'B'; // 'B' representa barco
+        }
+
+        return true;
     }
 }
