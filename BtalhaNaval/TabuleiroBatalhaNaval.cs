d6fe0fca45e5cd079f57f92669b4e47dfe05a4af
Gabriel Augusto <82222254+bielaugusto@users.noreply.github.com>
2023-06-29 10:49:43 -0300

    Update TabuleiroBatalhaNaval.cs

--- BtalhaNaval/TabuleiroBatalhaNaval.cs
+++ BtalhaNaval/TabuleiroBatalhaNaval.cs
@@ -2,85 +2,117 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-class TabuleiroBatalhaNaval
+namespace BatalhaNaval
 {
-    private int numLinhas;
-    private int numColunas;
-    private char[,] tabuleiro;
+    class TabuleiroBatalhaNaval
+    {
+        private int numLinhas;
+        private int numColunas;
+        private char[,] tabuleiro;
 
-    //Função para zerar o tabuleiro e settar como padrão 'A' para todas as posições
-    private void zeraMatriz(char[,] tabuleiro) {
-        for (int i = 0; i <= NumLinhas; i++)
+        //Função para zerar o tabuleiro e settar como padrão 'A' para todas as posições
+        private void zeraMatriz(char[,] tabuleiro)
         {
-            for (int j = 0; j <= numColunas; j++)
+            for (int i = 0; i <= numLinhas; i++)
             {
-                tabuleiro[i, j] = 'A'; //agua
+                for (int j = 0; j <= numColunas; j++)
+                {
+                    tabuleiro[i, j] = 'A'; //agua
+                }
             }
         }
-    }
 
 
-    public TabuleiroBatalhaNaval(int numLinhas, int numColunas)
-    {
-        this.numLinhas = numLinhas;
-        this.numColunas = numColunas;
-        tabuleiro = new char[numLinhas, numColunas];
-
-        zeraMatriz(tabuleiro);
-    }
-
-    public char[,] Tabuleiro { get; set; }
-    public int NumLinhas {
-        get { return numLinhas; }
-        set { numLinhas = value; }
-    }
-    public int NumColunas { 
-        get { return numColunas; }
-        set { numColunas = value; }
-    }
-
-    //Funções para imprimir os tabuleiros para os jogadores
-    public void imprimirTabuleiroJogador(char[,] tabuleiro) {
-        for (int i = 0; i <= numLinhas; i++)
+        public TabuleiroBatalhaNaval(int numLinhas, int numColunas)
         {
-            for (int j = 0; j <= numColunas; j++)
-            {
-                Console.WriteLine(tabuleiro[i, j] + " ");
-            }
-        }Console.WriteLine(" ");
-    }
+            this.numLinhas = numLinhas;
+            this.numColunas = numColunas;
+            tabuleiro = new char[numLinhas, numColunas];
 
-    public void imprimirTabuleiroAdversario(char[,] tabuleiro)
-    {
-        for (int i = 0; i <= numLinhas; i++)
-        {
-            for (int j = 0; j <= numColunas; j++)
-            {
-                Console.WriteLine(tabuleiro[i, j] + " ");
-            }
+            zeraMatriz(tabuleiro);
         }
-        Console.WriteLine(" ");
-    }
 
-    public bool AdicionarEmbarcacao(Embarcacao embarcacao, Posicao posicao)
-    {
-        // Verifica se a embarcação ultrapassa as dimensões do tabuleiro
-        if (posicao.Linha + embarcacao.Tamanho > numLinhas || posicao.Coluna + embarcacao.Tamanho > numColunas)
-            return false;
-
-        // Verifica se as posições onde a embarcação será adicionada já estão ocupadas
-        for (int i = 0; i < embarcacao.Tamanho; i++)
+        public char[,] Tabuleiro
         {
-            if (tabuleiro[posicao.Linha, posicao.Coluna + i] != 'A')
+            get { return tabuleiro; }
+            set { tabuleiro = value; }
+        }
+
+        public int NumLinhas
+        {
+            get { return numLinhas; }
+            set { numLinhas = value; }
+        }
+        public int NumColunas
+        {
+            get { return numColunas; }
+            set { numColunas = value; }
+        }
+
+        //Funções para imprimir os tabuleiros para os jogadores
+        public void imprimirTabuleiroJogador(char[,] tabuleiro)
+        {
+            for (int i = 0; i <= numLinhas; i++)
+            {
+                for (int j = 0; j <= numColunas; j++)
+                {
+                    Console.WriteLine(tabuleiro[i, j] + " ");
+                }
+            }
+            Console.WriteLine(" ");
+        }
+
+        public void imprimirTabuleiroAdversario(char[,] tabuleiro)
+        {
+            for (int i = 0; i <= numLinhas; i++)
+            {
+                for (int j = 0; j <= numColunas; j++)
+                {
+                    Console.WriteLine(tabuleiro[i, j] + " ");
+                }
+            }
+            Console.WriteLine(" ");
+        }
+
+        public bool AdicionarEmbarcacao(Embarcacao embarcacao, Posicao posicao)
+        {
+            // Verifica se a embarcação ultrapassa as dimensões do tabuleiro
+            if (posicao.Linha + embarcacao.Tamanho > numLinhas || posicao.Coluna + embarcacao.Tamanho > numColunas)
                 return false;
+
+            // Verifica se as posições onde a embarcação será adicionada já estão ocupadas
+            for (int i = 0; i < embarcacao.Tamanho; i++)
+            {
+                if (tabuleiro[posicao.Linha, posicao.Coluna + i] != 'A')
+                    return false;
+            }
+
+            // Adiciona a embarcação no tabuleiro
+            for (int i = 0; i < embarcacao.Tamanho; i++)
+            {
+                tabuleiro[posicao.Linha, posicao.Coluna + i] = 'B'; // 'B' representa barco
+            }
+
+            return true;
         }
 
-        // Adiciona a embarcação no tabuleiro
-        for (int i = 0; i < embarcacao.Tamanho; i++)
+
+        public bool RecebeuAtaque(Posicao posicao)
         {
-            tabuleiro[posicao.Linha, posicao.Coluna + i] = 'B'; // 'B' representa barco
+            // Verifica se a posição corresponde a uma embarcação no tabuleiro
+            if (tabuleiro[posicao.Linha, posicao.Coluna] != ' ')
+            {
+                // Embarcação atingida
+                tabuleiro[posicao.Linha, posicao.Coluna] = 'X'; // Marcar como atingida no tabuleiro
+                return true;
+            }
+            else
+            {
+                // Água, nenhum navio atingido
+                tabuleiro[posicao.Linha, posicao.Coluna] = '-'; // Marcar como água no tabuleiro
+                return false;
+            }
         }
 
-        return true;
     }
 }
